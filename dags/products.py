3cb5e4cdf3bb69c14e3c94c62b5a4374229d0158
scotty974 <fabienetheve@hotmail.com>
2026-09-19 22:12:09 +0200

    feat : add dockerfile to download dependecies

--- dags/products.py
+++ dags/products.py
@@ -1,22 +1,35 @@
 from airflow.sdk import dag, task, Variable
+from airflow.providers.postgres.hook.postgres import PostgresHook
 import datetime
 import requests
+from pathlib import Path
+import json
+
 
 
 @dag(start_date=datetime.datetime(2021,1,1),schedule="@daily")
 def ingest_dummy_products():
-    
+
     @task()
-    def get_products():
+    def get_products(**context)->str:
         URL = Variable.get("api_dummy_products", default=None)
-        
+        ds = context["ds"]
         try:
-            r = requests.get(URL)
-            return r.json()
+            r = requests.get(f"{URL}?limit=0", timeout=30).json()
+            path = f"/opt/airflow/data/bronze/products_{ds}.json"
+            Path(path).parent.mkdir(parents=True, exist_ok=True)
+            Path(path).write_text(json.dumps(r))
+            return path
         except requests.exceptions.RequestException as e:
             raise SystemExit(e)
+        
+        
+    @task()
+    def ingest_db(path:str):
+        
+        print(path)
+    
 
-
-    get_products()
+    ingest_db(get_products())
 
 ingest_dummy_products()
